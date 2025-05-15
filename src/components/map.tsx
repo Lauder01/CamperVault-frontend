1548f6e0b50f09107fb3aa7604b34f7ea1689330
Lauder01 <raulitoizg@gmail.com>
2025-05-15 19:06:50 +0200

    try example from website

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,29 +1,35 @@
-import 'mapbox-gl/dist/mapbox-gl.css';
 import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibGF1ZGVyIiwiYSI6ImNtYXBrZWlubDBoOHcya3MzcnlpZDU3bm4ifQ.hApMcQDp5sqiU-Q5eziAMA';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const Map = () => {
-  const mapContainer = useRef<HTMLDivElement>(null);
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+  const mapRef = useRef<mapboxgl.Map | null>(null);
 
   useEffect(() => {
-    if (!mapContainer.current) return;
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
+    mapboxgl.accessToken = 'pk.eyJ1IjoibGF1ZGVyIiwiYSI6ImNtYXBrZWlubDBoOHcya3MzcnlpZDU3bm4ifQ.hApMcQDp5sqiU-Q5eziAMA';
+
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current!,
       style: 'mapbox://styles/mapbox/satellite-v9',
-      center: [-3.7038, 40.4168],
-      zoom: 5,
+      projection: 'globe',
+      center: [137.915, 36.259],
+      zoom: 9
     });
-    return () => map.remove();
+
+    mapRef.current.on('style.load', () => {
+      mapRef.current!.setFog({});
+    });
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.remove();
+      }
+    };
   }, []);
 
-  return (
-    <div
-      ref={mapContainer}
-      style={{ width: '100%', height: '400px', borderRadius: '8px', overflow: 'hidden' }}
-    />
-  );
+  return <div ref={mapContainerRef} style={{ height: '100%' }} />;
 };
 
 export default Map;
