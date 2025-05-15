f5ed228fad65480c809b97e2d99e11f3d378e359
Lauder01 <raulitoizg@gmail.com>
2025-05-15 19:01:40 +0200

    only client execute map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,28 +1,27 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
-
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibGF1ZGVyIiwiYSI6ImNtYXBrZWlubDBoOHcya3MzcnlpZDU3bm4ifQ.hApMcQDp5sqiU-Q5eziAMA';
 
 const Map = () => {
   const mapContainer = useRef<HTMLDivElement>(null);
-  const map = useRef<mapboxgl.Map | null>(null);
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    setIsClient(true);
+  }, []);
 
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current as HTMLElement,
-      style: 'mapbox://styles/mapbox/satellite-v9', // estilo satélite
-      center: [-3.7038, 40.4168], // ejemplo: Madrid
+  useEffect(() => {
+    if (!isClient || !mapContainer.current) return;
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/satellite-v9',
+      center: [-3.7038, 40.4168],
       zoom: 5,
     });
-
-    return () => {
-      map.current?.remove();
-    };
-  }, []);
+    return () => map.remove();
+  }, [isClient]);
 
   return (
     <div
